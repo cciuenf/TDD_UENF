022324dfde2e2754a0e08a42cb5392f9516caccf
Élisson Michael <elissonmichael@yahoo.com.br>
2011-09-28 00:18:00 -0300

    sync

--- CampoMinado.rb
+++ CampoMinado.rb
@@ -4,10 +4,33 @@ class CampoMinado
 
   def initialize (tamanho)
     @tamanho = tamanho
+    @bombas = []
+
+    @tamanho.times do
+      @bombas << []
+    end
+
   end
 
+
+  def criaCampo()
+    for i in 0..@tamanho-1
+			for j in 0..@tamanho-1
+		    @bombas[i][j] = "#"
+			end
+		end
+
+  end
+
+
+
+  attr_accessor :bombas
   attr_reader :tamanho
 
 
 end
+
+#teste = CampoMinado.new(3)
+#teste.criaCampo
+#p teste.bombas
 
--- CampoMinado_spec.rb
+++ CampoMinado_spec.rb
@@ -8,9 +8,20 @@ describe CampoMinado do
     @jogo = CampoMinado.new(3)
 	end
 
-  it 'possui uma dimensao' do
-		@jogo.tamanho.should ==  3
+  it  'possui uma dimensao' do
+    @jogo.tamanho.should == 3
+  end
+
+  it  'possui um campo sem nada do tamanho certo' do
+    @jogo.bombas.should == [[],[],[]]
+  end
+
+  it 'cria um campo escondido' do
+    @jogo.criaCampo()
+    @jogo.bombas.should == [["#","#","#"],["#","#","#"],["#","#","#"]]
 	end
+
+
 
 end
 
