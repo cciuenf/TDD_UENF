20b9d00e844ad066865ce290fea94f0017c3df0b
Élisson Michael <elissonmichael@yahoo.com.br>
2011-09-28 01:02:49 -0300

    sync4

--- CampoMinado.rb
+++ CampoMinado.rb
@@ -5,19 +5,20 @@ class CampoMinado
   def initialize (tamanho)
     @tamanho = tamanho
     @bombas = []
+    @campo = []
 
     @tamanho.times do
       @bombas << []
+      @campo << []
     end
 
   end
-
 
   def criaCampo()
 
     for linha in 0..@tamanho-1
 			for coluna in 0..@tamanho-1
-		    @bombas[linha][coluna] = "#"
+		    @campo[linha][coluna] = "#"
 			end
 		end
 
@@ -27,8 +28,15 @@ class CampoMinado
     @bombas[linha][coluna] = "*"
   end
 
+  def clicaCasa(linha,coluna)
+    if (@bombas[linha][coluna] == "*")
+       @campo[linha][coluna] = "*"
+    else
+       @campo[linha][coluna] = "V"
+    end
+  end
 
-  attr_accessor :bombas
+  attr_accessor :bombas, :campo
   attr_reader :tamanho
 
 
--- CampoMinado_spec.rb
+++ CampoMinado_spec.rb
@@ -6,26 +6,33 @@ describe CampoMinado do
 
   before do
     @jogo = CampoMinado.new(3)
+    @jogo.criaCampo()
+    @jogo.adicionaBomba(1,1)
+	  @jogo.adicionaBomba(0,2)
 	end
 
-  it  'possui uma dimensao' do
+  it  'Possui uma dimensao' do
     @jogo.tamanho.should == 3
   end
 
-  it  'possui um campo sem nada do tamanho certo' do
-    @jogo.bombas.should == [[],[],[]]
+  it 'Cria um campo para esconder as bombas ' do
+    @jogo.campo.should == [["#","#","#"],["#","#","#"],["#","#","#"]]
+	end
+
+  it 'Testa se as bombas estao sendo colocadas no lugar certo' do
+    @jogo.bombas[1][1].should == "*"
+	  @jogo.bombas[0][2].should == "*"
+	end
+
+  it 'Testa o clique numa Bomba' do
+    @jogo.clicaCasa(1,1)
+    @jogo.campo[1][1].should == "*"
   end
 
-  it 'cria um campo escondido' do
-    @jogo.criaCampo()
-    @jogo.bombas.should == [["#","#","#"],["#","#","#"],["#","#","#"]]
-	end
-
-  it 'testa se a bomba foi colocada no lugar certo' do
-    @jogo.adicionaBomba(1,1)
-    @jogo.bombas[1][1].should == "*"
-	end
-
+  it 'Testa o clique numa casa sem bomba' do
+    @jogo.clicaCasa(2,2)
+    @jogo.campo[2][2].should == "V"
+  end
 
 end
 
